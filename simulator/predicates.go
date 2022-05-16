13dbfe1e095eb70753de791a262d2e1e1a39e182
xcming <326658575@qq.com>
2022-05-16 16:00:47 +0800

    fix: fix resource

--- simulator/predicates.go
+++ simulator/predicates.go
@@ -23,6 +23,7 @@ import (
 	"sync"
 
 	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	kube_util "k8s.io/autoscaler/cluster-autoscaler/utils/kubernetes"
